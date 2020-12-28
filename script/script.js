9ab5b181c8774406f1edc6ca53f550e0b5777767
hinatao3o <fjhrp0906@gmail.com>
2020-12-28 15:07:04 +0900

    scriptまとめた

--- script/script.js
+++ script/script.js
@@ -1,41 +1,59 @@
-// ページトップ
-$(function() {
-  let $offset = $('#nav').offset(),
-      navHeight = $('#nav').outerHeight();
-  $(window).scroll(function () {
-    if( $(window).scrollTop() > $offset.top) {
-      $('#nav').addClass('fixedNav');
-      $('body').css('padding-top', navHeight);
-      $('#page_top').stop().animate({'right': '50px'}, 300);
-    } else {
-      $('#nav').removeClass('fixedNav');
-      $('body').css('padding-top', 0);
-      $('#page_top').stop().animate({'right': '-50px'}, 300);
-    }
-  });
-});
-$('#page_top').click(function () {
-    $('body, html').animate({ scrollTop: 0 }, 500);
-    return false;
-});
+$(function () {
+    // ページトップ
+    let $offset = $('#nav').offset(),
+        navHeight = $('#nav').outerHeight();
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > $offset.top) {
+            $('#nav').addClass('fixedNav');
+            $('body').css('padding-top', navHeight);
+            $('#page_top').stop().animate({
+                'right': '50px'
+            }, 300);
+        } else {
+            $('#nav').removeClass('fixedNav');
+            $('body').css('padding-top', 0);
+            $('#page_top').stop().animate({
+                'right': '-50px'
+            }, 300);
+        }
+    });
+    $('#page_top').click(function () {
+        $('body, html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
 
-// ヘッダースムーススクロール
-$(function() {
-   $('a[href^="#"]').click(function(){
-    let href = $(this).attr('href');
-    let target = $(href == '#' || href === '' ? 'html' : href);
-    let position = target.offset().top-100;
-    $('html,body').animate({scrollTop : position}, 500);
-  });
+    // ヘッダースムーススクロール
+    $('a[href^="#"]').click(function () {
+        let href = $(this).attr('href');
+        let target = $(href == '#' || href === '' ? 'html' : href);
+        let position = target.offset().top - 100;
+        $('html,body').animate({
+            scrollTop: position
+        }, 500);
+    });
+
+    // 年齢を計算して入れる
+    (function() {
+        const age = getAge();
+        $('#age').text(age);
+    })();
 });
 
 // マーカー
-$(window).scroll(function (){
-  $('.marker-animation').each(function(){
-    let position = $(this).offset().top;
-    let scroll = $(window).scrollTop();
-    let windowHeight = $(window).height();
-
-    if (scroll > position - windowHeight) $(this).addClass('active');
-  });
+$(window).scroll(function () {
+    $('.marker-animation').each(function () {
+        let position = $(this).offset().top;
+        let scroll = $(window).scrollTop();
+        let windowHeight = $(window).height();
+        if (scroll > position - windowHeight) $(this).addClass('active');
+    });
 });
+
+function getAge() {
+    const birthday = '20030906';
+    let today = new Date();
+    let tdate = (today.getFullYear() * 10000) + ((today.getMonth() + 1) * 100) + today.getDate();
+    return Math.floor((tdate - birthday) / 10000);
+}
